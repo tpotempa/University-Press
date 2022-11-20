acff2011caed4c503606c35d857a7f20ce2355e5
tpotempa <tpotempa@gmail.com>
2022-11-20 21:27:55 +0100

    DB Update.

--- src/main/java/pl/edu/pwsztar/universitypress/repository/PaperBookRepository.java
+++ src/main/java/pl/edu/pwsztar/universitypress/repository/PaperBookRepository.java
@@ -7,6 +7,5 @@ import pl.edu.pwsztar.universitypress.model.PaperBook;
 
 public interface PaperBookRepository extends JpaRepository<PaperBook, Long> {
     List<PaperBook> findByTitleContaining(String title);
-
     List<PaperBook> findByPublishingDateBefore(LocalDate publishingDate);
 }
